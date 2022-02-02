9d528adf456471efb8f3636850afaf41b8136ee9
Gustavo Kazuo <kazucalado@gmail.com>
2022-02-02 15:01:56 -0300

    Tudo errado

--- src/App.js
+++ src/App.js
@@ -2,31 +2,60 @@ import React from "react";
 import "./App.css";
 
 import AppBar from "@mui/material/AppBar";
-
+import { styled } from "@mui/material/styles";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import { Box, Badge } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import MailIcon from "@mui/icons-material/Mail";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import InputBase from "@mui/material/InputBase";
+import { makeStyles } from "@mui/styles";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+const useStyles = makeStyles((theme) => ({
+  AppBar: {
+    paddingTop: 50,
+    paddingBottom: 50,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <AppBar position="static">
+      <AppBar position="static" color="inherit" className={classes.AppBar}>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <img src={"/images/branco.png"} />
         </Toolbar>
       </AppBar>
     </div>
